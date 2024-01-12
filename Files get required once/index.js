c223140c6873acd4dcc6fd1201c86f3d9485546f
DevHeris <badairoinioluwa578@gmail.com>
2024-01-12 05:02:55 +0200

    Files get required once

---
+++ Files get required once/index.js
@@ -0,0 +1,10 @@
+const counterObject = require("./script");
+
+console.log(counterObject.getCounter()); //0
+counterObject.increamentCounter();
+console.log(counterObject.getCounter()); //counter is now 1
+
+// Requiring from script.js again. will the counter variable automatically reset to 0 or be 1?
+const newCounterObject = require("./script");
+
+console.log(counterObject.getCounter()); // the result is 1
